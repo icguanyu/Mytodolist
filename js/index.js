93d476f6d8c36c47c188cc6de07afebea89e419c
icguanyu <icguanyu@gmail.com>
2018-06-03 00:33:46 +0800

    change vendor.js & use switch line-60

--- js/index.js
+++ js/index.js
@@ -4,93 +4,96 @@ var today = `${time.getFullYear()}.${time.getMonth()+1}.${time.getDate()}(${day_
 
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    newTodo: '', //輸入框
-    todos: [
-      {
-        id : '001',
-        title: '到大木博士家領取寶可夢',
-        time: today,
-        completed: false
-      }
-    ],
-    visi: 'all',
-    cacheTodo:{},
-    cacheTitle:''
-  },
-  methods:{
-    addTodo: function(){
-      var value = this.newTodo.trim();
-      var id = Math.floor(Date.now());
-      if(!value) return
-      this.todos.push({
-        id : id,
-        title: value,
-        time: today,
-        completed: false
-      })
-      this.newTodo = ''
+    el: '#app',
+    data: {
+        newTodo: '', //輸入框
+        todos: [{
+            id: '001',
+            title: '到大木博士家領取寶可夢',
+            time: today,
+            completed: false
+        }],
+        visi: 'all',
+        cacheTodo: {},
+        cacheTitle: ''
     },
-    removeTodo: function(index){
-      this.todos.splice(index,1)  
+    methods: {
+        stopPropagation: function(e) {
+            e.stopPropagation()
+        },
+        addTodo: function() {
+            var value = this.newTodo.trim();
+            var id = Math.floor(Date.now());
+            if (!value) return
+            this.todos.push({
+                id: id,
+                title: value,
+                time: today,
+                completed: false
+            })
+            this.newTodo = ''
+        },
+        removeTodo: function(index) {
+            this.todos.splice(index, 1)
+        },
+        editTodo: function(item) {
+            this.cacheTitle = item.title
+            this.cacheTodo = item
+        },
+        doneEdit: function(item) {
+            item.title = this.cacheTitle
+            this.cacheTodo = {}
+        },
+        cancelEdit: function() {
+            this.cacheTodo = {}
+        },
+        clearAll: function() {
+            var r = confirm('確認要清除所有代辦事項?')
+            if (r) {
+                this.todos = []
+            }
+        }
     },
-    editTodo: function(item){
-      this.cacheTitle = item.title
-      this.cacheTodo = item
-    },
-    doneEdit: function(item){
-      item.title = this.cacheTitle
-      this.cacheTodo = {}
-    },
-    cancelEdit: function(){
-      this.cacheTodo = {}
-    },
-    clearAll: function(){
-      var r = confirm('確認要清除所有代辦事項?')
-      if(r){
-        this.todos= []
-      }
+    computed: { //過濾後的資料
+        filter: function() {
+            var newTodos = [];
+            switch (this.visi) {
+                case 'ing':
+                    this.todos.forEach(function(item) {
+                        if (!item.completed) newTodos.push(item)
+                    })
+                    return newTodos
+                    break;
+                case 'done':
+                    this.todos.forEach(function(item) {
+                        if (item.completed) newTodos.push(item)
+                    })
+                    return newTodos
+                    break;
+                case 'all':
+                    return this.todos;
+                    break;
+            }
+        },
+        done: function() {
+            var done = []
+            done = this.todos.filter(function(item) {
+                return item.completed == true
+            })
+            return done.length
+        },
+        remain: function() {
+            var remain = []
+            remain = this.todos.filter(function(item) {
+                return item.completed == false
+            })
+            return remain.length
+        },
+        process: function() {
+            if (this.todos.length <= 0) {
+                return 0
+            }
+            return Math.floor(this.done / this.todos.length * 100) + '%'
+        }
     }
-  },
-  computed:{//過濾後的資料
-    filter: function(){
-      if(this.visi =='all'){
-        return this.todos;
-      }else if(this.visi =='ing') {
-        var newTodos = [];
-        this.todos.forEach(function(item){
-          if(!item.completed) newTodos.push(item)
-        })
-        return newTodos
-      }else if(this.visi =='done'){
-        var newTodos = [];
-        this.todos.forEach(function(item){
-          if(item.completed) newTodos.push(item)
-        })
-        return newTodos 
-      }
-      return [];
-    },
-    done: function(){
-      var done = []
-      done = this.todos.filter(function(item){
-        return item.completed == true
-      })
-      return done.length 
-    },
-    remain: function(){
-      var remain = []
-      remain = this.todos.filter(function(item){
-        return item.completed == false
-      })
-      return remain.length
-    },
-    process: function(){
-      if(this.todos.length<=0){
-        return 0
-      }
-      return Math.floor(this.done/this.todos.length*100) + '%'
-    }
-  }
 })
